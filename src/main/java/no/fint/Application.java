f76305445adc3f8cf1089d2dc0934e76e8ae8b52
Frode Sjovatsen <frode@fintlabs.no>
2022-04-04 13:00:54 +0200

    FC-81 #comment Upgraded deps #in-progress

--- src/main/java/no/fint/Application.java
+++ src/main/java/no/fint/Application.java
@@ -1,13 +1,11 @@
 package no.fint;
 
-import com.github.springfox.loader.EnableSpringfox;
 import no.fint.oauth.OAuthConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableSpringfox
 @EnableScheduling
 @Import(OAuthConfig.class)
 @SpringBootApplication
